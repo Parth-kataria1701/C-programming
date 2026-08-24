f5c825335d956507d6f49d27281c057f994865de
Parth-kataria1701 <Parth.34654@stu.upes.ac.in>
2026-08-24 23:10:06 +0530

    Write a program to calculate the factorial of a number

---
+++ q29.c
@@ -0,0 +1,17 @@
+//Write a program to calculate the factorial of a number.
+#include <stdio.h>
+
+int main() {
+    int n;
+    long long factorial = 1;
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+
+    printf("%lld", factorial);
+
+    return 0;
+}
