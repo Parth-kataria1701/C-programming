36b80227f797bc5b4cb9fc0804b73ad4cb7d4db0
Parth-kataria1701 <Parth.34654@stu.upes.ac.in>
2026-09-16 21:32:24 +0530

    Check if a matrix is symmetric.

---
+++ q76.c
@@ -0,0 +1,38 @@
+//Check if a matrix is symmetric.
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    int a[100][100];
+    int symmetric = 1;
+
+    scanf("%d %d", &r, &c);
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    if (r != c) {
+        symmetric = 0;
+    } else {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (a[i][j] != a[j][i]) {
+                    symmetric = 0;
+                    break;
+                }
+            }
+            if (symmetric == 0)
+                break;
+        }
+    }
+
+    if (symmetric)
+        printf("True");
+    else
+        printf("False");
+
+    return 0;
+}
